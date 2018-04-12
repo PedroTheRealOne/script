deabfb24b9494ae96b86cc8b9b81ad5a6e2c7ace
Pedro <pedrotherealone1@gmail.co>
2018-04-12 11:53:25 -0300

    1 Step
    Getting all informations (unchecked check boxes)

--- app.js
+++ app.js
@@ -31,49 +31,30 @@ var obj = {
 
 
     //Card Vingadores
-    await page.click('#board > div:nth-child(1) > div > div.list-cards.u-fancy-scrollbar.u-clearfix.js-list-cards > a:nth-child(1) > div.list-card-details');
+    await page.goto('https://trello.com/c/yvhOZt8W/1-vingadores');
+
 
     await page.addScriptTag({
         content: `
-        function waifu(){
-            return $('.checklist-item')[0].innerText
+        function getInfo(){
+            return $('.checklist-item')[2].innerText
+        }
+        function getInfo1(){
+            return $('.checklist-item')[3].innerText
+        }    
+        function getInfo2(){
+            return $('.checklist-item')[4].innerText
+        }    
+        function getInfo3(){
+            return $('.checklist-item')[5].innerText
         }
         `
     });
 
-    var result = await page.evaluate('waifu()');
-
-    console.log(result)
-
-
-    await page.click('#classic > div.window-overlay > div > div > a');
-    //
-
-
-    //Card Matrix
-    await page.click('#board > div:nth-child(1) > div > div.list-cards.u-fancy-scrollbar.u-clearfix.js-list-cards > a:nth-child(2) > div.list-card-details');
-    
-
-    await page.click('#classic > div.window-overlay > div > div > a');
-    //
-
-
-    //Card Linguagem
-    await page.click('#board > div:nth-child(2) > div > div.list-cards.u-fancy-scrollbar.u-clearfix.js-list-cards > a:nth-child(1)');
-
-    await page.click('#classic > div.window-overlay > div > div > a');
-    //
-
-
-    //Card Fuse
-    await page.click('#board > div:nth-child(2) > div > div.list-cards.u-fancy-scrollbar.u-clearfix.js-list-cards > a:nth-child(2)');
-
-    await page.click('#classic > div.window-overlay > div > div > div > div.window-main-col > div.card-detail-data.u-gutter > div.card-detail-item.card-detail-item-block.u-clearfix > div > div.current.markeddown.hide-on-edit.js-card-desc.js-show-with-desc > p > a');
-
-    //
-
-
-
-
+    var result = await page.evaluate('getInfo()');
+    var result1 = await page.evaluate('getInfo1()');
+    var result2 = await page.evaluate('getInfo2()');
+    var result3 = await page.evaluate('getInfo3()');
+    console.log(result, result1, result2, result3);
 
 })();

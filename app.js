f6f44d8e2294e1cc680e1407aea6c80e0632361c
Pedro <pedrotherealone1@gmail.co>
2018-04-13 17:32:10 -0300

    ADDS

--- app.js
+++ app.js
@@ -56,5 +56,60 @@ var obj = {
     var result2 = await page.evaluate('getInfo2()');
     var result3 = await page.evaluate('getInfo3()');
     console.log(result, result1, result2, result3);
+    //
+
+
+    //Card Matrix
+
+    await page.goto('https://trello.com/c/Fz1m2BAv/2-matrix');
+
+    await page.addScriptTag({
+        content: `
+        
+
+        `
+
+    });
+    //
+
+
+    //Card Fuse
+    await page.goto('https://trello.com/c/nNItV8D8/8-fuse');
+
+    await browser.newPage('https://www.fusetools.com/');
+    //
+
+
+    //Card Linguagens
+    await page.goto('https://trello.com/c/6ddfGj9v/4-linguagens');
+
+    await page.addScriptTag({
+        content:`
+        function getChecked(){
+            return $('.checklist-item')[0].innerText
+        }
+        function getChecked1(){
+            return $('.checklist-item')[1].innerText
+        }
+        function getChecked2(){
+            return $('.checklist-item')[5].innerText
+        }
+        function getChecked3(){
+            return $('.checklist-item')[6].innerText
+        }
+        function getChecked4(){
+            return $('.checklist-item')[7].innerText
+        }
+        `
+    });
+
+    var info = await page.evaluate('getChecked()');
+    var info1 = await page.evaluate('getChecked1()');
+    var info2 = await page.evaluate('getChecked2()');
+    var info3 = await page.evaluate('getChecked3()');
+    var info4 = await page.evaluate('getChecked4()');
+    console.log(info, info1, info2, info3, info4);
+    //
+
 
 })();
